cb11d0302d3d7700a48313b430e10f7f28cb2e6e
NAMAN V SRIVASTAV <78561560+NamanVSrivastav@users.noreply.github.com>
2024-12-23 18:14:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Hamburger Menu Functionality
-function hamburg() {
-    const navbar = document.querySelector(".dropdown");
-    navbar.style.transform = "translateY(0px)";
-}
-
-function cancel() {
-    const navbar = document.querySelector(".dropdown");
-    navbar.style.transform = "translateY(-500px)";
-}
 
 // Smooth Scrolling for Navigation Links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
